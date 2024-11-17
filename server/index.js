e9344d7b483394395a6af35180673ef98d16c354
AJIN  VR <130544695+Ajinvr@users.noreply.github.com>
2024-11-17 09:56:52 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 connectDB();
 
 const corsOptions = {
-  origin: [process.env.FRONTEND_URL, 'http://localhost:5173'],
+  origin: [process.env.FRONTEND_URL,'https://secretswap.vercel.app', 'http://localhost:5173'],
   credentials: true,
 };
 
